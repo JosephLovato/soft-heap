7ff50a2e81e2bed2a7279058fda5cbf8d0de8b7f
sddale <seth.d.dale@gmail.com>
2022-11-05 12:55:45 -0600

    Removed commented-out old code

--- include/policies.hpp
+++ include/policies.hpp
@@ -1,10 +1,8 @@
 #pragma once
 #include <concepts>
-// #include <type_traits>
 
 namespace soft_heap::policy {
 
-// concept Arithmetic = std::is_arithmetic_v<T>;
 template <class T>
 concept TotalOrdered = requires(T param) { requires std::totally_ordered<T>; };
 
