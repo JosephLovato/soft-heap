74cc0748403fc1a207b298fbc6ca3d336c9cac72
sddale <seth.d.dale@gmail.com>
2022-11-21 16:34:43 -0700

    Added concept for a totalordered STL-type container

--- include/policies.hpp
+++ include/policies.hpp
@@ -1,9 +1,48 @@
 #pragma once
 #include <concepts>
+#include <iterator>
+#include <string>
+#include <vector>
 
 namespace soft_heap::policy {
 
 template <class T>
 concept TotalOrdered = requires(T param) { requires std::totally_ordered<T>; };
+
+template <class ContainerType>
+concept TotalOrderedContainer =
+    requires(ContainerType a, const ContainerType b) {
+      requires std::totally_ordered<ContainerType>;
+      requires std::regular<ContainerType>;
+      requires std::swappable<ContainerType>;
+      requires std::destructible<typename ContainerType::value_type>;
+      requires std::same_as<typename ContainerType::reference,
+                            typename ContainerType::value_type &>;
+      requires std::same_as<typename ContainerType::const_reference,
+                            const typename ContainerType::value_type &>;
+      requires std::forward_iterator<typename ContainerType::iterator>;
+      requires std::forward_iterator<typename ContainerType::const_iterator>;
+      requires std::signed_integral<typename ContainerType::difference_type>;
+      requires std::same_as<
+          typename ContainerType::difference_type,
+          typename std::iterator_traits<
+              typename ContainerType::iterator>::difference_type>;
+      requires std::same_as<
+          typename ContainerType::difference_type,
+          typename std::iterator_traits<
+              typename ContainerType::const_iterator>::difference_type>;
+      { a.begin() } -> std::same_as<typename ContainerType::iterator>;
+      { a.end() } -> std::same_as<typename ContainerType::iterator>;
+      { b.begin() } -> std::same_as<typename ContainerType::const_iterator>;
+      { b.end() } -> std::same_as<typename ContainerType::const_iterator>;
+      { a.cbegin() } -> std::same_as<typename ContainerType::const_iterator>;
+      { a.cend() } -> std::same_as<typename ContainerType::const_iterator>;
+      { a.size() } -> std::same_as<typename ContainerType::size_type>;
+      { a.max_size() } -> std::same_as<typename ContainerType::size_type>;
+      { a.empty() } -> std::same_as<bool>;
+      { a.back() } -> std::totally_ordered;
+    };
+static_assert(TotalOrderedContainer<std::vector<int>>);
+static_assert(TotalOrderedContainer<std::string>);
 
 }  // namespace soft_heap::policy
